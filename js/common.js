3c44d63f15e7cbea1e4303a42442f31b41efd241
hhj <gkagus98@naver.com>
2026-01-30 17:33:06 +0900

    order전까지

---
+++ js/common.js
@@ -0,0 +1,13 @@
+$(function () {
+  $("#headers").load("/common/header.html", function (res, status) {
+    if (status === "error") {
+      console.error("헤더 로드 실패");
+    }
+  });
+
+  $("#footers").load("/common/footer.html", function (res, status) {
+    if (status === "error") {
+      console.error("푸터 로드 실패");
+    }
+  });
+});
