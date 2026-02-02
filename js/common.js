e19dc52e63d432229c437f2868c7719fff3fba9a
sms <thdprincess@naver.com>
2026-02-02 16:24:53 +0900

    load(v1-1)

--- js/common.js
+++ js/common.js
@@ -1,11 +1,11 @@
 $(function () {
-  $("#headers").load("/common/header.html", function (res, status) {
+  $("#headers").load("common/header.html", function (res, status) {
     if (status === "error") {
       console.error("헤더 로드 실패");
     }
   });
 
-  $("#footers").load("/common/footer.html", function (res, status) {
+  $("#footers").load("common/footer.html", function (res, status) {
     if (status === "error") {
       console.error("푸터 로드 실패");
     }
